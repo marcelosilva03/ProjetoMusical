1992bd526df64add1a25a7e0acfdf59a0ab9fcba
marcelosilva03 <106699447+marcelosilva03@users.noreply.github.com>
2024-09-06 12:01:18 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,61 +1,75 @@
 let compositores = [
     {
+        instagram:"https://www.instagram.com/hanszimmer/?hl=pt",
         titulo: "Hans Zimmer",
         descrição: "Hans Zimmer: O Mestre das Trilhas Sonoras",
         biografia: "Hans Zimmer é um compositor alemão, reconhecido mundialmente por suas trilhas sonoras memoráveis para filmes. Nascido em Frankfurt, Zimmer iniciou sua carreira na música pop, mas encontrou seu verdadeiro chamado na composição para cinema. Suas obras se destacam pela fusão de elementos eletrônicos com orquestrações grandiosas, criando atmosferas únicas e emocionantes.",
-        link: "https://pt.wikipedia.org/wiki/Hans_Zimmer"
+        link: "https://pt.wikipedia.org/wiki/Hans_Zimmer",
+        tags: "interestelar espacial trilha sonora cinema eletrônica contemporâneo contemporaneo"
     },
     {
+        instagram:"https://www.instagram.com/beethovenclassics/",
         titulo: "Beethoven",
         descrição: "Beethoven: O Pai da Sinfonia",
         biografia: "Ludwig van Beethoven foi um compositor alemão, considerado um dos mais influentes da história da música clássica. Surdo na maior parte de sua vida, Beethoven revolucionou a música com suas sinfonias, sonatas e concertos, que expressam uma profundidade emocional e uma complexidade técnica sem precedentes.",
-        link: "https://pt.wikipedia.org/wiki/Ludwig_van_Beethoven"
+        link: "https://pt.wikipedia.org/wiki/Ludwig_van_Beethoven",
+        tags: "classica sinfonia romântico estoicismo viena school" 
     },
     {
+        instagram: "https://www.instagram.com/reels/audio/629411778142572/?locale=pt_BR&hl=am-et",
         titulo: "Yasuharu Takanashi",
         descrição: "Yasuharu Takanashi: O Mestre da Animação",
         biografia: "Yasuharu Takanashi é um compositor japonês, famoso por suas trilhas sonoras para animes. Suas obras são conhecidas por sua versatilidade, abrangendo desde temas emocionantes e épicos até melodias mais leves e relaxantes. Takanashi é responsável por trilhas de animes populares como Naruto e Fairy Tail.",
-        link: "https://ja.wikipedia.org/wiki/髙梨康治"
+        link: "https://ja.wikipedia.org/wiki/髙梨康治",
+        tags: "anime jpop ost fairy tail" 
     },
     {
+        instagram: "https://www.instagram.com/pyotr_tchaikovsky/",
         titulo: "Pyotr Ilyich Tchaikovsky",
         descrição: "Tchaikovsky: O Poeta da Música Clássica",
         biografia: "Pyotr Ilyich Tchaikovsky foi um dos mais importantes compositores russos. Suas obras, marcadas por um romantismo intenso e melodias cativantes, o tornaram um dos compositores clássicos mais populares do mundo. Algumas de suas obras mais conhecidas incluem o balé 'O Quebra-Nozes' e a ópera 'Eugene Onegin'.",
-        link: "https://pt.wikipedia.org/wiki/Pyotr_Ilyich_Tchaikovsky"
+        link: "https://pt.wikipedia.org/wiki/Pyotr_Ilyich_Tchaikovsky",
+        tags: "clássica balé romântico russo"
     },
     {
+        instagram: "https://www.instagram.com/antonio_vivaldi_oficial/",
         titulo: "Antonio Vivaldi",
         descrição: "Vivaldi: O Mestre do Violino",
         biografia: "Antonio Vivaldi foi um compositor italiano do período Barroco, conhecido por suas obras para violino e por suas inovações na escrita musical. Suas 'Quatro Estações' são uma das obras mais famosas da música clássica e um exemplo brilhante de sua maestria na composição.",
-        link: "https://pt.wikipedia.org/wiki/Antonio_Vivaldi"
+        link: "https://pt.wikipedia.org/wiki/Antonio_Vivaldi",
+        tags: "violino concerto italiano"
     },
     {
         instagram: "https://www.instagram.com/mozartclassics/",
         titulo: "Wolfgang Amadeus Mozart",
         descrição: "Mozart: O Gênio Musical Austríaco",
         biografia: "Wolfgang Amadeus Mozart foi um prodígio musical que compôs uma vasta quantidade de obras em diversos gêneros musicais. Sua música é caracterizada por uma elegância e perfeição formal que o tornaram um dos compositores mais influentes de todos os tempos. Algumas de suas obras mais conhecidas incluem as óperas 'As Bodas de Figaro' e 'Don Giovanni'.",
-        link: "https://pt.wikipedia.org/wiki/Wolfgang_Amadeus_Mozart"
+        link: "https://pt.wikipedia.org/wiki/Wolfgang_Amadeus_Mozart",
+        tags: "clássica ópera opera prodígio viena school"
     },
     {
         instagram: "https://www.instagram.com/bachclassicals/",
         titulo: "Johann Sebastian Bach",
         descrição: "Bach: O Pai da Música Alemã",
         biografia: "Johann Sebastian Bach foi um dos maiores compositores barrocos. Sua música é caracterizada por uma complexidade contrapontística e uma profunda espiritualidade. Bach exerceu uma enorme influência sobre os compositores que o sucederam e é considerado um dos pilares da música ocidental. Algumas de suas obras mais conhecidas incluem o 'Cravo Bem Temperado' e as 'Suítes para Orquestra'.",
-        link: "https://pt.wikipedia.org/wiki/Johann_Sebastian_Bach"
+        link: "https://pt.wikipedia.org/wiki/Johann_Sebastian_Bach",
+        tags: "contraponto organ piano alemao alemão"
     },
     {
         instagram: "https://www.instagram.com/maestro.thiagosantos/reel/C-DdIQBAtA5/",
         titulo: "Compositor Barroco", 
         descrição: "O Barroco: Época de Grande Expressividade",
         biografia: "O período barroco foi marcado por uma grande complexidade musical e uma busca por emoções intensas. Compositores barrocos como Bach e Handel dominaram a Europa com suas obras.",
-        link: "https://pt.wikipedia.org/wiki/Barroco_(música)"
+        link: "https://pt.wikipedia.org/wiki/Barroco_(música)",
+        tags: "barroco música clássica classica expressividade europa"
     },
     {
         instagram: "https://www.instagram.com/fschubertfilh/",
         titulo: "Franz Schubert",
         descrição: "Schubert: O Mestre da Lied",
         biografia: "Franz Schubert foi um compositor austríaco do período clássico e romântico, conhecido por suas belas melodias e profundo lirismo. Suas obras mais famosas incluem os ciclos de lieder, como 'A Bela Moça do Lago' e 'A Troutinha'.",
-        link: "https://pt.wikipedia.org/wiki/Franz_Schubert"
+        link: "https://pt.wikipedia.org/wiki/Franz_Schubert",
+        tags: "romântico lied piano viena school"
     },
 ]
 
